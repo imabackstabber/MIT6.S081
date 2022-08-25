2056bb07e16a135880c861b74e2b89ecd937a187
imabackstabber <312276423@qq.com>
2022-08-25 20:51:22 +0800

    finish vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,36 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+_vmprint(pagetable_t pgtbl,int depth)
+{
+    for(int i = 0;i < 512;i++){
+      pte_t pte = pgtbl[i];
+      if((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X)) == 0){
+        // points to a low-level pgtbl
+        for(int j = 0;j < depth;j++){
+          if(j) 
+            printf(" ");
+          printf("..");
+        }
+        printf("%d: pte %p pa %p\n",i,pte,PTE2PA(pte));
+        uint64 child = PTE2PA(pte);
+        _vmprint((pagetable_t)child,depth+1);
+      } else if(pte & PTE_V){
+        for(int j = 0;j < depth;j++){
+          if(j) 
+            printf(" ");
+          printf("..");
+        }
+        printf("%d: pte %p pa %p\n",i,pte,PTE2PA(pte)); 
+      }
+    }
+}
+
+void
+vmprint(pagetable_t pgtbl)
+{
+  printf("page table %p\n",pgtbl);
+  _vmprint(pgtbl,1);  
+}
